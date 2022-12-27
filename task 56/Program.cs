e832be90e3a549df3902c2f097f63b0f1c4fd0d2
Elizaveta1094 <treninskaya.elizaveta@yandex.ru>
2022-12-27 10:01:42 +0300

    hw8

---
+++ task 56/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++)
+
+{
+matrix[i,j] = new Random().Next(0,11);
+Console.Write(matrix[i,j] + " ");
+
+}
+Console.WriteLine();
+}
+Console.WriteLine(matrix.GetLength(1));
+int minPosition = Int32.MaxValue;
+int index = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = sum + matrix[i, j];
+    }
+     if (sum < minPosition)
+    {
+        minPosition = sum;
+        index++;
+    }
+}
+
+Console.WriteLine("строка с наименьшей суммой элементов под номером: " + (index) + ", с суммой элементов равной: " + (minPosition));
+    
+
+Console.ReadLine();
---
+++ task 60/Program.cs
@@ -0,0 +1,33 @@
+﻿//...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив,
+//  добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] FillMatrix(int[,,] matrix)
+{
+    int[,,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1), matrix.GetLength(2)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                newMatrix[i, j, z] = new Random().Next(1, 100);
+                if(newMatrix[i,j,z] == newMatrix[i,j,z])
+                {
+                    newMatrix[i,j,z] = new Random().Next(1, 100);
+                }
+                Console.Write(newMatrix[i, j, z] + $"({i},{j},{z})" + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    return newMatrix;
+}
+
+int[,,] matrix = new int[2, 2, 2];
+FillMatrix(matrix);
